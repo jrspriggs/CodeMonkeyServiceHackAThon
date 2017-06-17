d06de4c7b771d3c5a0e8a23ef126b69860ac82eb
Cheryl Dodd <Cheryl.Dodd@LibertyMutual.com>
2017-06-17 15:24:35 -0400

    removed helper method from value object

--- src/main/java/com/lmig/ci/policy/services/codemonkey/vo/ClassCodeSearch.java
+++ src/main/java/com/lmig/ci/policy/services/codemonkey/vo/ClassCodeSearch.java
@@ -1,6 +1,5 @@
 package com.lmig.ci.policy.services.codemonkey.vo;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class ClassCodeSearch {
@@ -25,17 +24,5 @@ public class ClassCodeSearch {
 	}
 	public void setSicCodes(List<String> sicCodes) {
 		this.sicCodes = sicCodes;
-	}
-	
-	public List<String> getShortSicCodes() {
-		List<String> shortSicCodes = new ArrayList<String>();
-		for (String sicCode : this.sicCodes) {	
-			if (sicCode.length() > 4) {
-				shortSicCodes.add(sicCode.substring(0,5));
-			} else {
-				shortSicCodes.add(sicCode);
-			}
-		}
-		return shortSicCodes;
 	}
 }
